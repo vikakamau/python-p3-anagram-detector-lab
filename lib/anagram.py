89ffd94e4b022ea73fd6f97a381b5b809c1cfbe1
victor kamau <vikakamau04@gmail.com>
2024-05-29 19:37:03 +0300

    initial

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+      return [word for word in words if sorted(word) == sorted(self.word)]
+        
+
+
+        
